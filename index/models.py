70dc1b9a2b139cbc0ecb85f7b92ff01b52541c21
AnyukiOyster <anyukioyster@gmail.com>
2024-12-02 20:27:43 +0500

    First

---
+++ index/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+# Создаём таблицу категорий товаров
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+
+# Создаём таблицу для товаров
+class Product(models.Model):
+    product_name = models.CharField(max_length=256)
+    product_desc = models.TextField()
+    product_price = models.FloatField()
+    product_count = models.IntegerField()
+    product_photo = models.ImageField()
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+#Таблица корзины
+class Cart(models.Model):
+    user_id = models.IntegerField()
+    user_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_pr_count = models.IntegerField()
