48d8079d9f17718b6d72a2b02315b6c510a3b38e
AnyukiOyster <anyukioyster@gmail.com>
2024-12-06 21:30:12 +0500

    Lesson 06/12

--- index/views.py
+++ index/views.py
@@ -1,11 +1,6 @@
-from itertools import product
-
 from django.shortcuts import render
-from unicodedata import category
-
 from .models import Category, Product, Cart
 
-# Create your views here.
 #Главная страница
 def home_page(request):
     #Достаём данные из БД
@@ -15,3 +10,23 @@ def home_page(request):
     context = {'products': products, 'categories': categories}
 
     return render(request, 'home.html', context)
+
+#Вывод товаров по выбранной категории
+def category_page(request, pk):
+    #выводим нужную категорию
+    category = Category.objects.get(id=pk)
+    #Фильтруем товары по категории
+    products = Product.objects.filter(product_category=category)
+    #Передаём данные на FE
+    context = {'category': category, 'products': products}
+
+    return render(request, 'category.html', context)
+
+#Вывод определённого товара
+def product_page(request,pk):
+    #Вывод выбранного товара
+    product = Product.objects.get(id=pk)
+    # Передаём данные на FE
+    context = {'product': product}
+
+    return render(request, 'product.html', context)
