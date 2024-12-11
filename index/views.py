44080d646f9806e16d6ad83e6da97c9037212d13
AnyukiOyster <anyukioyster@gmail.com>
2024-12-11 21:21:10 +0500

    Class 11/12

--- index/views.py
+++ index/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Product, Cart
+from .forms import RegForm
+from django.views import View
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+
 
 #Главная страница
 def home_page(request):
@@ -30,3 +35,40 @@ def product_page(request,pk):
     context = {'product': product}
 
     return render(request, 'product.html', context)
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        context = {'form': RegForm}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegForm(request.POST)
+
+        #Проверка корректности данных
+        if form.is_valid():
+            username = form.clean_username()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password2')
+
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+
+            #Авторизация пользователя
+            login(request, user)
+            return redirect('/')
+
+        # Если данные некорректны
+        context = {'form': RegForm, 'message': 'Неверный адрес электронной почты или пароль'}
+        return render(request, self.template_name, context)
+
+def search(request):
+    if request.method == 'POST':
+        get_product = request.POST.get('search')
+
+        if Product.objects.get(product_name__iregex=get_product):
+            search_product = Product.objects.get(product_name__iregex=get_product)
+            return redirect(f'/product/{search_product.id}')
+        else:
+            return redirect('/')
